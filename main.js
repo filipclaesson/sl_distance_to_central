0c17e4a66df89ef70503c1520f539fd07d277f09
filipclaesson <filipclaesson@hotmail.com>
2017-04-08 15:03:57 +0200

    adding new query for getting non existing geo lcations

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var Apartments = "";
 var geoLocationsToLookup = [];
 var SLGeodata = [];
 
-Postgres.runQuery("select distinct lon_short as lon,lat_short as lat from view_of_apt_sl where avg_time_to_central is null", db, function(data){ 
+Postgres.runQuery("select distinct lon_short as lon,lat_short as lat from view_of_apt_sl v where v.avg_time_to_central is null and not exists(select 1 from exclude_sl e where v.lon_short = e.lon_short and v.lat_short = e.lat_short)", db, function(data){ 
 
 	console.log(data.data)
 	geoLocationsToLookup = data.data
--- queries/data.sql
+++ queries/data.sql
@@ -1,4 +1,5 @@
 ------- get non linked apartments locations ------
 
-select distinct lon_short as lon,lat_short as lat from view_of_apt_sl where avg_time_to_central is null
+select distinct lon_short as lon,lat_short as lat from view_of_apt_sl v where v.avg_time_to_central is null
+and not exists(select 1 from exclude_sl e where v.lon_short = e.lon_short and v.lat_short = e.lat_short)
 ---------- -----------------
